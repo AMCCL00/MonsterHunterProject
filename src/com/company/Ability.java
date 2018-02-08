0a9df6aa3217bae96221fae13a6af8bb04f7bf63
Andrew McClary <andrew.mccalry00@gmail.com>
2018-02-08 16:49:26 -0500

    Linked my monster and ability variables

--- src/com/company/Ability.java
+++ src/com/company/Ability.java
@@ -4,4 +4,26 @@ public class Ability extends Monster{
 
     private String abilityName;
     private int abilityDamage;
+
+    public Ability(String abilityName, int abilityDamage, String name, int Health) {
+        super(name,Health);
+        this.abilityName = abilityName;
+        this.abilityDamage = abilityDamage;
+    }
+
+    public String getAbilityName() {
+        return abilityName;
+    }
+
+    public void setAbilityName(String abilityName) {
+        this.abilityName = abilityName;
+    }
+
+    public int getAbilityDamage() {
+        return abilityDamage;
+    }
+
+    public void setAbilityDamage(int abilityDamage) {
+        this.abilityDamage = abilityDamage;
+    }
 }
--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -3,4 +3,25 @@ package com.company;
 public class Monster {
     private String name;
     private int Health;
+
+    public Monster(String name, int health) {
+        this.name = name;
+        Health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return Health;
+    }
+
+    public void setHealth(int health) {
+        Health = health;
+    }
 }
